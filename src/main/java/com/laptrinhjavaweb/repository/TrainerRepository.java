fe531ea7a1ae3debe1cffa58ad8be5684f7b4de4
daotanhai <haidtgcd19802@gmail.com>
2021-06-15 15:17:17 +0700

    update repository

--- src/main/java/com/laptrinhjavaweb/repository/TrainerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.laptrinhjavaweb.repository;
-
-import com.laptrinhjavaweb.entity.TrainerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainerRepository extends JpaRepository<TrainerEntity, Long> {
-    TrainerEntity findTrainerEntityByUserName(String userName);
-}
--- src/main/java/com/laptrinhjavaweb/repository/TrainingStaffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.laptrinhjavaweb.repository;
-
-import com.laptrinhjavaweb.entity.TrainingStaffEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainingStaffRepository extends JpaRepository<TrainingStaffEntity, Long> {
-    TrainingStaffEntity findTrainingStaffEntityByUserName(String userName);
-}
